ea65a8715430494da52385620d4f2756dcb59919
Thomas Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2017-01-11 19:13:55 +0100

    example.py

--- ImArray/example.py
+++ ImArray/example.py
@@ -19,7 +19,6 @@ The author may be contacted @
 thomas.vuillaume@lapp.in2p3.fr
 """
 
-
 import geometry as geo
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,55 +33,52 @@ import vizualisation as viz
 
 MEAN = np.empty(0)
 
-
-#If you want some plots to be done
+# If you want some plots to be done
 BoolPlot = True
 noise = 0
-
 
 '''
 Shower parameters
 '''
-impact_point = np.array([0,250,0])
+impact_point = np.array([0, 250, 0])
 
-#shower direction
+# shower direction
 salt = math.radians(74)
 saz = math.radians(0)
 
-#pointing direction
+# pointing direction
 talt = math.radians(74)
 taz = math.radians(0)
 
-#shower parameters
-s_top = [0,0,15000]
-s_bot = [0,0,1000]
-s_center = [0,0,8000]
+# shower parameters
+s_top = [0, 0, 15000]
+s_bot = [0, 0, 1000]
+s_center = [0, 0, 8000]
 stop = 15000
 slength = 12000
 swidth = 200
 
 npoints = 500
 
-
-#shower = obj.linear_segment(s_top, s_bot, npoints)
+# shower = obj.linear_segment(s_top, s_bot, npoints)
 shower = obj.random_ellipsoide(stop, slength, swidth, salt, saz, impact_point, npoints)
 
 '''
 Load a telescope configuration
 '''
 
-tel_normal = geo.altaz_to_normal(talt,taz)
+tel_normal = geo.altaz_to_normal(talt, taz)
 
-tel1 = geo.Telescope([500,100,-100],tel_normal,0)
-tel2 = geo.Telescope([-10,300,100],tel_normal,0)
-tel3 = geo.Telescope([-500,400,10],tel_normal,0)
-tel4 = geo.Telescope([-350,-500,0],tel_normal,0)
-tel5 = geo.Telescope([600,-100,150],tel_normal,0)
-tel6 = geo.Telescope([300,-300,50],tel_normal,0)
-tel7 = geo.Telescope([80,0,0],tel_normal,0)
-tel8 = geo.Telescope([-80,0,0],tel_normal,0)
-tel9 = geo.Telescope([0,-80,0],tel_normal,0)
-tel10 = geo.Telescope([0,80,0],tel_normal,0)
+tel1 = geo.Telescope([500, 100, -100], tel_normal, 0)
+tel2 = geo.Telescope([-10, 300, 100], tel_normal, 0)
+tel3 = geo.Telescope([-500, 400, 10], tel_normal, 0)
+tel4 = geo.Telescope([-350, -500, 0], tel_normal, 0)
+tel5 = geo.Telescope([600, -100, 150], tel_normal, 0)
+tel6 = geo.Telescope([300, -300, 50], tel_normal, 0)
+tel7 = geo.Telescope([80, 0, 0], tel_normal, 0)
+tel8 = geo.Telescope([-80, 0, 0], tel_normal, 0)
+tel9 = geo.Telescope([0, -80, 0], tel_normal, 0)
+tel10 = geo.Telescope([0, 80, 0], tel_normal, 0)
 
 alltel = [tel1, tel2, tel3, tel4, tel5, tel6]
 # alltel = geo.load_telescopes("data/tel_pos.dat")
@@ -94,32 +90,22 @@ for tel1 in alltel:
         if ((tel1.center == tel2.center).all() and tel1.id != tel2.id):
             print(tel1.id, tel2.id, tel1.center, tel2.center)
 
-
-
-ERR = np.empty(0)
-#for n in range(10):
-#    shower = obj.random_ellipsoide(15000, slength, swidth, salt, saz, impact_point, npoints)
-
 if BoolPlot:
-    viz.plot_shower3d(shower,alltel)
-
-IM = []
-coord = []
-vis = []
-
-plt.xkcd()
+    viz.plot_shower3d(shower, alltel)
 
 start = time.time()
 
+# file to save images
 if not os.path.isdir('results'):
     os.mkdir('results')
-f = open('results/teldata.xml','w')
-
+f = open('results/teldata.xml', 'w')
 f.write("\n")
 f.write("<subarray>\n")
 
-#SIZE SHOULD CHANGE WITH CAMERA TYPE
-allhist = np.zeros(1855)
+allhist = np.zeros(1855)  ### SIZE SHOULD CHANGE WITH CAMERA TYPE ###
+IM = []
+coord = []
+vis = []
 HillasParameters = []
 
 for tel in alltel:
@@ -130,54 +116,55 @@ for tel in alltel:
         IM.append(im)
         center_camera = geo.camera_center(tel)
         if geo.is_point_in_camera_plane(im, tel):
-            vis.append(geo.is_point_visible(im,tel))
-        im_cam = geo.site_to_camera_cartesian([im[0],im[1],im[2]], tel)
+            vis.append(geo.is_point_visible(im, tel))
+        im_cam = geo.site_to_camera_cartesian([im[0], im[1], im[2]], tel)
         X.append(im_cam[0])
         Y.append(im_cam[1])
-    #hist = ci.camera_image(tel, np.column_stack((X,Y)), filename="results/{}.txt".format(tel.id))
-    hist = ci.camera_image(tel, np.column_stack((X,Y)), "results/{}.txt".format(tel.id), noise)
-    print(hist)
-    hp = hillas.hillas_parameters_2(hist[:,0], hist[:,1], hist[:,2])
-    hp.append(tel.id)
+    # hist = ci.camera_image(tel, np.column_stack((X,Y)), filename="results/{}.txt".format(tel.id))
+    hist = ci.camera_image(tel, np.column_stack((X, Y)), "results/{}.txt".format(tel.id), noise)
+    # print(hist)
 
+    # Compute the Hillas parameters for each image:
+    hp = hillas.hillas_parameters_2(hist[:, 0], hist[:, 1], hist[:, 2])
+    hp.append(tel.id)
     HillasParameters.append(hp)
 
-    allhist += hist[:,2]
-    alt,az = geo.normale_to_altaz(tel.normale)
-    f.write('<telescope telId="%d" position="%f,%f,%f" dirAlt="%f" dirAz="%f" focal="%f">\n' % (tel.id,tel.center[0],tel.center[1],tel.center[2],alt,az,tel.focale))
+    allhist += hist[:, 2]
+    alt, az = geo.normale_to_altaz(tel.normale)
+
+    # save the images:
+    f.write('<telescope telId="%d" position="%f,%f,%f" dirAlt="%f" dirAz="%f" focal="%f">\n' % (
+    tel.id, tel.center[0], tel.center[1], tel.center[2], alt, az, tel.focale))
     for xyph in hist:
-        f.write('%f,%f,%f;\n' % (xyph[0],xyph[1],xyph[2]))
+        f.write('%f,%f,%f;\n' % (xyph[0], xyph[1], xyph[2]))
     f.write('</telescope>\n')
 
     if BoolPlot:
-        plt.plot(X,Y,'o', label = tel.center,markersize=3)
+        plt.plot(X, Y, 'o', label=tel.center, markersize=3)
 
-
+# Hillas geometrical reconstruction:
 pa = hillas.impact_parameter_average(alltel, HillasParameters, alt, az)
 p = hillas.impact_parameter_ponderated(alltel, HillasParameters, alt, az)
+
 print("Real impact parameter : ", impact_point)
-print("Reco with simple average = %s \tError = %.2fm" % (pa, math.sqrt(((impact_point-pa)**2).sum())))
-print("Reco with ponderation and cut = %s \tError = %.2fm" % (p, math.sqrt(((impact_point-p)**2).sum())))
-ERR = np.append(ERR, math.sqrt(((impact_point-p)**2).sum()))
-#print(ERR)
-np.savetxt('results/hillas.txt',HillasParameters,fmt='%.5f',
-    header="size\t<x>\t<y>\tl\tw\tr\tphi\tpsi\tmiss\tTelId")
+print("Reco with simple average = %s \tError = %.2fm" % (pa, math.sqrt(((impact_point - pa) ** 2).sum())))
+print("Reco with ponderation and cut = %s \tError = %.2fm" % (p, math.sqrt(((impact_point - p) ** 2).sum())))
 
-hist[:,2] = allhist
-np.savetxt('results/all.txt',hist,fmt='%.5f')
+# save the results:
+np.savetxt('results/hillas.txt', HillasParameters, fmt='%.5f',
+           header="size\t<x>\t<y>\tl\tw\tr\tphi\tpsi\tmiss\tTelId")
 
+hist[:, 2] = allhist
+np.savetxt('results/all.txt', hist, fmt='%.5f')
 
-#print('\nElapsed (ms):', (time.time() - start) * 1000)
+# print('\nElapsed (ms):', (time.time() - start) * 1000)
 
 f.write('\n</subarray>')
 f.close()
 
-
 if BoolPlot:
-    plt.legend(loc='upper center', fancybox=True, ncol=3, bbox_to_anchor=(0.5,1.1))
+    plt.xkcd()
+    plt.legend(loc='upper center', fancybox=True, ncol=3, bbox_to_anchor=(0.5, 1.1))
     plt.show()
-print(ERR)
-MEAN = np.append(MEAN, ERR.mean())
-print(MEAN)
 
-plt.show()
+plt.close()
