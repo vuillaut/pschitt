05c7f7082ba776d7c738b6819d686d0dd24825a4
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2017-01-19 10:03:13 +0100

    display the pointing directions of the telescopes

--- ImArray/vizualisation.py
+++ ImArray/vizualisation.py
@@ -21,3 +21,56 @@ def plot_shower3d(shower,alltel):
         art3d.pathpatch_2d_to_3d(p, z=tel.center[2], zdir="z")
     plt.axis([-1000, 1000, -1000, 1000])
     plt.show()
+
+
+def display_camera_image(telescope, histogram):
+    """
+    display an image of the camera of the telescope
+    Parameters
+    ----------
+    telescope : telescope class
+    histogram : histogram of the signal in each pixel
+    """
+    X, Y = np.loadtxt(telescope.pixpos_filename, unpack = True)
+    plt.scatter(X,Y,c=histogram)
+    plt.axis('equal')
+    plt.show()
+    return X,Y,histogram
+
+
+def display_pointing_tel(tel, show=True):
+    """
+    Display the pointing direction of a telescope as seen from above
+    Parameters
+    ----------
+    tel: telescope class
+    show: Boolean, to display or not
+    """
+    ax = plt.axes()
+    ax.arrow(tel.camera_center[0], tel.camera_center[1], tel.normal[0], tel.normal[1], head_width=5, head_length=10, fc='k', ec='k')
+    #plt.xlim([np.min([start[0], end[0]]), np.max([start[0], end[0]])])
+    #plt.ylim([np.min([start[1], end[1]]), np.max([start[1], end[1]])])
+    if show:
+        plt.show()
+
+
+def display_pointing_array(alltel):
+    """
+    Display the pointing direction of each telescope in the array as seen from above
+
+    Parameters
+    ----------
+    alltel: list of telescope classes
+    """
+    centers = np.array([tel.center for tel in alltel])
+    xmin = centers[:,0].min() - 50
+    xmax = centers[:,0].max() + 50
+    ymin = centers[:,1].min() - 50
+    ymax = centers[:,1].max() + 50
+
+    for tel in alltel:
+        display_pointing_tel(tel, show=False)
+
+    plt.axis('equal')
+    plt.axis([xmin,xmax,ymin,ymax])
+    plt.show()
